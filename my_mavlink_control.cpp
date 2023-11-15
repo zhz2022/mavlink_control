6b7ec691f8f5d32c999ff2fbd9b23daddbcb1da8
zhangpeng  <zhangpengbjut@hotmail.com>
2023-11-15 16:29:48 +0800

    modify:reduce wait time

--- my_mavlink_control.cpp
+++ my_mavlink_control.cpp
@@ -273,7 +273,7 @@ void mode_takeoff(Autopilot_Interface &autopilot_interface,mavlink_set_position_
 	// NOW pixhawk will try to move
 
 	// Wait for 8 seconds, check position
-	for (int i=0; i < 8; i++)
+	for (int i=0; i < 2; i++)
 	{
 		mavlink_local_position_ned_t pos = autopilot_interface.current_messages.local_position_ned;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
@@ -289,7 +289,7 @@ void mode_move_forward(Autopilot_Interface &autopilot_interface,mavlink_set_posi
 	// SEND THE COMMAND
 	autopilot_interface.update_setpoint(sp);
     // Wait for 4 seconds, check position
-	for (int i=0; i < 4; i++)
+	for (int i=0; i < 2; i++)
 	{
 		mavlink_local_position_ned_t pos = autopilot_interface.current_messages.local_position_ned;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
@@ -305,7 +305,7 @@ void mode_move_backward(Autopilot_Interface &autopilot_interface,mavlink_set_pos
 	// SEND THE COMMAND
 	autopilot_interface.update_setpoint(sp);
     // Wait for 4 seconds, check position
-	for (int i=0; i < 4; i++)
+	for (int i=0; i < 2; i++)
 	{
 		mavlink_local_position_ned_t pos = autopilot_interface.current_messages.local_position_ned;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
@@ -321,7 +321,7 @@ void mode_move_left(Autopilot_Interface &autopilot_interface,mavlink_set_positio
 	// SEND THE COMMAND
 	autopilot_interface.update_setpoint(sp);
 	// Wait for 4 seconds, check position
-	for (int i=0; i < 4; i++)
+	for (int i=0; i < 2; i++)
 	{
 		mavlink_local_position_ned_t pos = autopilot_interface.current_messages.local_position_ned;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
@@ -337,7 +337,7 @@ void mode_move_right(Autopilot_Interface &autopilot_interface,mavlink_set_positi
 	// SEND THE COMMAND
 	autopilot_interface.update_setpoint(sp);
 	// Wait for 4 seconds, check position
-	for (int i=0; i < 4; i++)
+	for (int i=0; i < 2; i++)
 	{
 		mavlink_local_position_ned_t pos = autopilot_interface.current_messages.local_position_ned;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
@@ -353,7 +353,7 @@ void mode_stop(Autopilot_Interface &autopilot_interface,mavlink_set_position_tar
 	// SEND THE COMMAND
 	autopilot_interface.update_setpoint(sp);
 	// Wait for 4 seconds, check position
-	for (int i=0; i < 4; i++)
+	for (int i=0; i < 2; i++)
 	{
 		mavlink_local_position_ned_t pos = autopilot_interface.current_messages.local_position_ned;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
